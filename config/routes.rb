0df97771938b336926878927a99e4da615fdf350
Ryo-exid <sakura4oneok@gmail.com>
2021-05-04 08:55:01 +0000

    [Finish]応用課題5/Bookers2へ「検索機能」を実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'homes#top'
   get 'home/about' => 'homes#about'
-  
-  get 'search' => 'searches#search'
+
+  get 'search' => 'search#search'
 
   resources :books do
     resource :favorites, only: [:create, :destroy]
