33f92a3c3ea34ca467beb6621ae4cf1ea269ee2c
Ryo-exid <sakura4oneok@gmail.com>
2021-05-07 08:45:52 +0000

    [Fix]応用課題8/Welcomeメイラーからthanksメイラーに名前を修正

--- app/mailers/thanks_mailer.rb
+++ app/mailers/thanks_mailer.rb
@@ -1,7 +1,7 @@
-class WelcomeMailer < ApplicationMailer
+class ThanksMailer < ApplicationMailer
   default from: 'notifications@example.com'
 
-  def welcome_email
+  def thanks_email
     @user = params[:user]
     mail(to: @user.email, subject: '会員登録が完了しました。')
   end
