08e25914263091ff296ee02a8212f4738d21c5fc
Ryo-exid <sakura4oneok@gmail.com>
2021-05-07 11:23:20 +0000

    [Fix]メール送信機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => {
-   :registrations => 'users/registrations',
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
   }
 
   root 'homes#top'
