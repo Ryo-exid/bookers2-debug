c3b2cb5cdd3640df867023d059c75d4dc2a00b54
Ryo-exid <sakura4oneok@gmail.com>
2021-05-16 13:37:23 +0000

    [Fix]応用課題10

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,7 +24,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = '自分のアプリからの発信用gmailアドレス'
+  config.mailer_sender = '自分のアプリの発信用gmailアドレス'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
